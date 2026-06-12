ab83e7034abd196c6851d861e16460eb966060db
CharukaLakmal98 <charukalakmal920@gmail.com>
2026-06-12 18:59:51 +0530

    added cucumber.plugin as a key

--- automation-framework/src/test/java/runners/TestRunner.java
+++ automation-framework/src/test/java/runners/TestRunner.java
@@ -5,12 +5,10 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
-
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = "cucumber.glue", value = "stepdefinitions")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm")
+@ConfigurationParameter(key = "cucumber.plugin", value = "pretty, io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm")
 public class TestRunner {
 }
